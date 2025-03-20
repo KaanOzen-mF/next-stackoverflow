b1051807861cd6f70fdb8e92cac844e51ffddca1
KaanOzen-mF <kaanozn98@gmail.com>
2025-03-20 11:51:50 +0300

    Add sidebarLinks constant for navigation sidebar

---
+++ constants/index.ts
@@ -0,0 +1,60 @@
+// Define and export an array of sidebar link objects used for navigation.
+// Each object contains properties for the icon image URL, the route, and the label.
+export const sidebarLinks = [
+  {
+    // Icon for Home page
+    imgURL: "/icons/home.svg",
+    // Route for Home page
+    route: "/",
+    // Display label for Home page
+    label: "Home",
+  },
+  {
+    // Icon for Community section
+    imgURL: "/icons/users.svg",
+    // Route for Community section
+    route: "/community",
+    // Display label for Community section
+    label: "Community",
+  },
+  {
+    // Icon for Collections section
+    imgURL: "/icons/star.svg",
+    // Route for Collections section
+    route: "/collection",
+    // Display label for Collections section
+    label: "Collections",
+  },
+  {
+    // Icon for Jobs section
+    imgURL: "/icons/suitcase.svg",
+    // Route for Find Jobs page
+    route: "/jobs",
+    // Display label for Jobs section
+    label: "Find Jobs",
+  },
+  {
+    // Icon for Tags section
+    imgURL: "/icons/tag.svg",
+    // Route for Tags page
+    route: "/tags",
+    // Display label for Tags section
+    label: "Tags",
+  },
+  {
+    // Icon for Profile section
+    imgURL: "/icons/user.svg",
+    // Route for Profile page; further dynamic parts can be appended later (e.g., /profile/1)
+    route: "/profile",
+    // Display label for Profile section
+    label: "Profile",
+  },
+  {
+    // Icon for Ask a Question page
+    imgURL: "/icons/question.svg",
+    // Route for the Ask a Question page
+    route: "/ask-question",
+    // Display label for Ask a Question
+    label: "Ask a question",
+  },
+];
